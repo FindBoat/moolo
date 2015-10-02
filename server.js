c14f23319bd04c027e1084c1a935b62209829898
FindBoat <sgzhaohang@gmail.com>
2015-10-02 16:51:52 -0700

    nothing

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app, port;
 app = require('./.app/server/app');
 
 port = app.port;
-
 app.listen(port, function() {
   return console.log("Listening on " + port + "\nPress CTRL-C to stop server.");
 });
